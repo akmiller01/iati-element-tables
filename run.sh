c06e339267398814efcb25768c216385311ac07b
akmiller01 <alex.k.miller@gmail.com>
2021-04-14 12:20:21 -0400

    All priority publishers

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -d venv ]; then
+  python3 -m virtualenv venv
+  source venv/bin/activate
+  pip install -r requirements.txt
+else
+  source venv/bin/activate
+fi
+
+while read pub; do
+  echo $pub && \
+  python3 publisher_data.py $pub;
+done < publishers.txt
